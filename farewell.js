0c33314c96695adf5b84ef9f63d60582c959eea0
Sandra Tourjman <sandratourjman@Sandras-MacBook-Pro.local>
2019-03-20 22:40:15 -0500

    First commit

---
+++ farewell.js
@@ -0,0 +1,15 @@
+exports.farewellEnglish = function(){
+ console.log("Goodbye");
+}
+
+exports.farewellJapanese = function(){
+ console.log("Sayōnara");
+}
+
+exports.farewellSpanish = function(){
+	console.log("Adiós");
+}
+
+exports.farewellIcelandic = function(){
+	console.log("Bless");
+}
---
+++ main.js
@@ -0,0 +1,26 @@
+const greetings = require("./greeting.js");
+const farewells = require("./farewell.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Halló"
+greetings.greetingIcelandic();
+
+// "Goodbye"
+farewells.farewellEnglish();
+
+// "Sayōnara"
+farewells.farewellJapanese();
+
+// "Adiós"
+farewells.farewellSpanish();
+
+// "Bless"
+farewells.farewellIcelandic();
